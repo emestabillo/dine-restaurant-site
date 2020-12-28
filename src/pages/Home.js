df78d0cab0f66881ccde635033c6e056f9b9df0f
emestabillo <emsestabillo@gmail.com>
2020-12-27 22:51:02 -0500

    Add styles for mobile about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Header from "../components/Header";
 import HeaderText from "../components/HeaderText";
-import Container from "../components/Container";
+// import Container from "../components/Container";
 import CTA from "../components/CTA";
+import About from "../components/About";
 
 // import logo from "../images/logo.svg";
 
 function Home() {
   return (
-    <Header hero="homeHeader">
-      <Container>
-        <div className="hero">
-          <HeaderText
-            title="Exquisite dining since 1989"
-            subtitle="Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse"
-          ></HeaderText>
-          <CTA ctaColor="cta--dark" ctaText="Book a table" />
+    <>
+      <Header hero="homeHeader">
+        <div className="container">
+          <div className="hero">
+            <HeaderText
+              title="Exquisite dining since 1989"
+              subtitle="Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse"
+            ></HeaderText>
+            <CTA ctaColor="cta--dark" ctaText="Book a table" />
+          </div>
         </div>
-      </Container>
-    </Header>
+      </Header>
+      <About />
+    </>
   );
 }
 
