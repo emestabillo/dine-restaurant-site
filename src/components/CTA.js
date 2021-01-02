ebf9b70a76dafcdd0f39c6c7243c24dc42da41ac
Emmilie Estabillo <emsestabillo@gmail.com>
2021-01-01 22:23:52 -0500

    Cta styles for larger screens

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom"
 export default function CTA() {
   return (
     <section className="cta">
-      <h2 className="cta__heading">Ready to make a reservation?</h2>
-      <Link to='/booking' className="btn btn--dark">Book a table</Link>
+      <div className="container">
+        <div className="cta__text">
+          <h2 className="cta__heading">Ready to make a reservation?</h2>
+          <Link to='/booking' className="btn btn--dark">Book a table</Link>
+        </div>       
+      </div>    
     </section>
   )
 }
