7653f4b9bbf6c51ff77fa6ee8d353ad8d037e711
Emmilie Estabillo <emsestabillo@gmail.com>
2021-01-01 22:04:19 -0500

    Footer styles for bigger screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,33 @@
 import React from "react"
+import logo from "../images/logo.svg";
+import {Link} from "react-router-dom";
 
 export default function Footer () {
   return (
-    <div>This is the footer</div>
+    <section className="footer">
+      <div className="container">
+        <div className="footer__text">
+          <Link to="/">
+            <img src={logo} className="logo" alt="dine logo" />
+          </Link>
+          <div className="footer__details">
+            <p className="footer__address">
+                Marthwaite, Sedbergh
+                <br/>
+                Cumbria
+                <br/>
+                +00 44 123 4567
+            </p>
+            <p className="footer__hours">
+              Open times
+              <br/>
+              Mon - Fri: 9:00 am - 10:00 pm
+              <br/>
+              Sat - Sun: 9:00 am - 11:30 pm
+            </p>
+          </div>            
+        </div>
+      </div>
+    </section>
   )
 }
