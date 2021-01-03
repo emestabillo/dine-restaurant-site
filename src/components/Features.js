419e6f6d39516e4edea21a88f6ea93944e36c3ae
Emmilie Estabillo <emsestabillo@gmail.com>
2021-01-02 20:15:42 -0500

    ADd picture tag to handle image changes

--- src/components/Features.js
+++ src/components/Features.js
@@ -9,27 +9,35 @@ function Features() {
   return (
     <section className="features">
       <div className="container">
-        <div className="btn-container">
-        {items.map((item, index) => {
-          return (
-            <button 
-              key={item.id}
-              onClick={() => setFeatureInfo(index)}>
-              {item.title}
-            </button>
-          )
-        } )}
-        </div>
-      </div>
-      <article>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <img 
-        srcSet={`assets/${imgName}-mobile.jpg 375w, assets/${imgName}-tablet.jpg 768w, assets/${imgName}-desktop.jpg 992w`}
-        // sizes="(max-width: 767px) 375vw, (max-width: 991px) 991vw, 992vw"
-        src={`assets/${imgName}-mobile.jpg`} alt="family gathering"/>
-       {/* <img src={require('../images/homepage/family-gathering-mobile.jpg').default} alt={title}/> */}
-      </article>
+        <article>
+        {/* <img 
+          srcSet={`assets/${imgName}-mobile.jpg 326w, assets/${imgName}-tablet.jpg 573w, assets/${imgName}-desktop.jpg 540w`}
+          sizes="(min-width: 992px) 50vw, 100vw"
+        src={`assets/${imgName}-mobile.jpg`} alt="family gathering"/> */}
+          
+          <picture>
+            <source media="(max-width:767px)" srcSet={`assets/${imgName}-mobile.jpg`}/>
+            <source media="(max-width:991px)" srcSet={`assets/${imgName}-tablet.jpg`}/>
+            <img src={`assets/${imgName}-desktop.jpg`} alt={title}s/>
+          </picture>
+
+          <div className="features__info">
+            <div className="btn-container">
+              {items.map((item, index) => {
+                return (
+                  <button 
+                    key={item.id}
+                    onClick={() => setFeatureInfo(index)}>
+                    {item.title}
+                  </button>
+                )} 
+              )}
+            </div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </div>
+        </article>
+      </div>    
     </section>
   )
 }
--- src/data.js
+++ src/data.js
@@ -4,28 +4,19 @@ export const items = [
     id: 1,
     title: 'family gathering',
     description: 'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.',
-    imgName: 'family-gathering',
-    // imgMob: './images/homespage/family-gathering-mobile.jpg',
-    // imgTab: './images/homespage/family-gathering-tablet.jpg',
-    // imgDesk: './images/homespage/family-gathering-desktop.jpg',
+    imgName: 'family-gathering'
   },
   {
     id: 2,
     title: 'special events',
     description: 'Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.',
-    imgName: 'special-events',
-    // imgMob: './images/homespage/special-events-mobile.jpg',
-    // imgTab: './images/homespage/special-events-tablet.jpg',
-    // imgDesk: './images/homespage/special-events-desktop.jpg',
+    imgName: 'special-events'
   },
  
   {
    id: 3,
     title: 'social events',
     description: 'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.',
-    imgName: 'social-events',
-    // imgMob: '/images/homespage/social-events-mobile.jpg',
-    // imgTab: './images/homespage/social-events-tablet.jpg',
-    // imgDesk: './images/homespage/social-events-desktop.jpg',
+    imgName: 'social-events'
   },
 ];
