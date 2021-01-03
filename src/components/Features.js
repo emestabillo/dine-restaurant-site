824486448fde62b26190f082c2591cde33888f8a
Emmilie Estabillo <emsestabillo@gmail.com>
2021-01-02 19:10:38 -0500

    Add assets to public folder and logic for dynamic images

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,7 +4,7 @@ import { items } from '../data';
 function Features() {
   const [featureInfo, setFeatureInfo] = useState(0);
 
-  const {title, description, imgMob} = items[featureInfo]
+  const {title, description, imgName} = items[featureInfo]
 
   return (
     <section className="features">
@@ -24,7 +24,11 @@ function Features() {
       <article>
         <h2>{title}</h2>
         <p>{description}</p>
-        <img src={require('../images/homepage/family-gathering-mobile.jpg').default} alt={title}/>
+        <img 
+        srcSet={`assets/${imgName}-mobile.jpg 375w, assets/${imgName}-tablet.jpg 768w, assets/${imgName}-desktop.jpg 992w`}
+        // sizes="(max-width: 767px) 375vw, (max-width: 991px) 991vw, 992vw"
+        src={`assets/${imgName}-mobile.jpg`} alt="family gathering"/>
+       {/* <img src={require('../images/homepage/family-gathering-mobile.jpg').default} alt={title}/> */}
       </article>
     </section>
   )
