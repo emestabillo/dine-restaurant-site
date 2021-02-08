50cb3c263dcf8e622c611839f7ce4749ecba3772
Emmilie Estabillo <emsestabillo@gmail.com>
2021-02-08 11:26:34 -0500

    Add custom hook for bg images

---
+++ src/components/useWindowWidth.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth ] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+        return () => window.removeEventListener('resize', handleWindowResize);
+    },[]);
+
+    return windowWidth;
+}
+
+export default useWindowWidth
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,28 @@ import CTA from "../components/CTA";
 import About from "../components/About";
 import { MenuList } from "../components/MenuList";
 import Button from "../components/Button"
+import useWindowWidth from "../components/useWindowWidth"
+import homeHeaderDesk from "../assets/homepage/hero-bg-desktop.jpg"
+import homeHeaderTab from "../assets/homepage/hero-bg-tablet.jpg"
+import homeHeaderMob from "../assets/homepage/hero-bg-mobile.jpg"
 
 function Home() {
+  const windowWidth = useWindowWidth();
+
+  const imageUrl = () => {
+    if (windowWidth >= 992) {
+      return homeHeaderDesk
+    } else if 
+      (windowWidth >= 768) {
+        return homeHeaderTab
+    } else {
+      return homeHeaderMob
+    }
+  }
+
   return (
     <>
-      <Header hero="homeHeader">
+      <Header hero="homeHeader" style={{ backgroundImage: `{url(${imageUrl})}`}}>
         <HeaderText
           title='Exquisite dining since 1989'
           subtitle="Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.">
